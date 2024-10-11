3876b53aea4df3bd5d2d0ee961b59307f5850504
Jay Sawant <164754866+JaySawant2707@users.noreply.github.com>
2024-10-11 16:23:08 +0530

    Added new player character
    added knight girl character with animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,38 +1,41 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-//using System.Numerics;
-using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movements")]
     [SerializeField] float moveSpeed = 10f;
     [SerializeField] float jumpSpeed = 10f;
     [SerializeField] float climbSpeed = 3f;
+    [Header("Projectile")]
     [SerializeField] GameObject bullet;
     [SerializeField] Transform gun;
+
+    bool isAlive = true;
     float playerGravity;
+    bool isGrounded = false;
+    public bool isFacingRight = true;
     Vector2 runInput;
-    Rigidbody2D rb2d;
+    Rigidbody2D rb;
     Animator animator;
     CapsuleCollider2D myFeetCollider;
     BoxCollider2D myBodyCollider;
-    public bool isAlive = true;
 
     void Start()
     {
-        rb2d = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         myFeetCollider = GetComponent<CapsuleCollider2D>();
         myBodyCollider = GetComponent<BoxCollider2D>();
-        playerGravity = rb2d.gravityScale;
+        playerGravity = rb.gravityScale;
     }
 
     void Update()
     {
         if (!isAlive) return;
+
+        GroundCheck();
+
         Run();
         Flip();
         ClimbLadder();
@@ -48,11 +51,18 @@ public class PlayerController : MonoBehaviour
     void OnJump(InputValue value)
     {
         if (!isAlive) return;
-        if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+
+        if (!isGrounded) { return; }
+
         if (value.isPressed)
         {
-            rb2d.velocity += new Vector2(0f, jumpSpeed);
+            Jump();
         }
+    }
+
+    void Jump()
+    {
+        rb.velocity += new Vector2(0f, jumpSpeed);
     }
 
     void OnFire(InputValue value)
@@ -66,10 +76,10 @@ public class PlayerController : MonoBehaviour
 
     void Run()
     {
-        Vector2 playerVelocity = new Vector2(runInput.x * moveSpeed, rb2d.velocity.y);
-        rb2d.velocity = playerVelocity;
+        Vector2 playerVelocity = new Vector2(runInput.x * moveSpeed, rb.velocity.y);
+        rb.velocity = playerVelocity;
 
-        bool playerHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;    //Check if player is moving horizontally or not
+        bool playerHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;    //Check if player is moving or not (horizontally)
         animator.SetBool("isRunning", playerHorizontalSpeed);
 
     }
@@ -78,24 +88,33 @@ public class PlayerController : MonoBehaviour
     {
         if (!myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Climbing")))
         {
-            rb2d.gravityScale = playerGravity;
+            rb.gravityScale = playerGravity;
             animator.SetBool("isClimbing", false);
             return;
         }
-        rb2d.gravityScale = 0f; //player will not fall from ladder when gravity is 0
-        rb2d.velocity = new Vector2(rb2d.velocity.x, runInput.y * climbSpeed);
+        rb.gravityScale = 0f; //player will not fall from ladder when gravity is 0.
+        rb.velocity = new Vector2(rb.velocity.x, runInput.y * climbSpeed);
 
-        bool playerVerticalSpeed = Mathf.Abs(rb2d.velocity.y) > Mathf.Epsilon;  //Check if player is moving Vertically or not
+        bool playerVerticalSpeed = Mathf.Abs(rb.velocity.y) > Mathf.Epsilon;  //Check if player is moving or not (Vertically)
         animator.SetBool("isClimbing", playerVerticalSpeed);
     }
 
     void Flip()
     {
-        bool playerHorizontalSpeed = Mathf.Abs(rb2d.velocity.x) > Mathf.Epsilon;    //Check if player is moving horizontally or not
+        bool playerHorizontalSpeed = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;    //Check if player is moving or not (horizontally)
 
         if (playerHorizontalSpeed)
         {
-            transform.localScale = new Vector2(Mathf.Sign(rb2d.velocity.x), 1f);
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
+
+        if (transform.localScale.x < 0)
+        {
+            isFacingRight = false;
+        }
+        else
+        {
+            isFacingRight = true;
         }
     }
 
@@ -107,5 +126,10 @@ public class PlayerController : MonoBehaviour
             animator.SetTrigger("Dying");
             FindObjectOfType<GameSession>().ProcessPlayerDeath();
         }
+    }
+
+    void GroundCheck()
+    {
+        isGrounded = myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground"));
     }
 }
